742df8d437eaff4d627a3adbb1b3af8a985d41d1
Filipe Ferreira <filipe.ferreira@pontual.pt>
2024-11-07 10:56:39 +0000

    + Commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProductListComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
